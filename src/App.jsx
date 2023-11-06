f3d5662cfe92cd88ea6996b18b86e6d0cdf3ef1b
natejmaster@gmail.com <natejmaster@gmail.com>
2023-11-06 12:56:18 -0600

    Got connection up and running between component features and App server

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,18 @@
-import { useState, useEffect } from 'react';
-//Replace these with my actual components
-import SearchBar from './components/SearchBar';
-import IssueList from './components/IssueList';
-import './App.css'
+import { Outlet } from 'react-router-dom';
+import Header from './components/Header'
+import Footer from './components/Footer'
+import './App.css';
 
 function App() {
   return (
     <>
-      <Header>
-        <Navigation />
-      </Header>
-      <Project />
-      <Footer />
+      <div>
+        <Header />
+        <main>
+          <Outlet />
+        </main>
+        <Footer />
+      </div>
     </>
   );
 }
---
+++ src/components/Footer.jsx
@@ -0,0 +1,15 @@
+const Footer = () => {
+    return (
+      <footer >
+        <h4>Contact Me</h4>
+        <div>
+          <a>GitHub:</a>
+          <a>Email:</a>
+          <a>Phone:</a>
+          <a>Linkedin</a>
+        </div>
+      </footer>
+    );
+  }
+
+  export default Footer;
